b9b843fb5d1487453f129dbf7157639a5d72406e
M O H I T  K U M A R <mohitkr.9933@gmail.com>
2024-12-03 01:17:13 +0530

    change some colors

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -55,7 +55,7 @@ const Todo = () => {
     <div className='flex items-center my-7 bg-gray-200 rounded-full'>
       <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2
       placeholder:text-slate-600' type='text' placeholder='Add your task'/>
-      <button onClick={add} className='border-none rounded-full bg-teal-300 w-32 h-14 text-lg font-medium cursor-pointer'>Add +</button>
+      <button onClick={add} className='border-none rounded-full bg-blue-500 text-white w-32 h-14 text-lg font-medium cursor-pointer'>Add +</button>
     </div>
 
     <div>
