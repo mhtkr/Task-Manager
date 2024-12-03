e4eb3c69a4f097a40c5589306e7ab0f2aa13163c
M O H I T  K U M A R <mohitkr.9933@gmail.com>
2024-12-03 23:36:52 +0530

    Added description-box and improve the UI

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -3,15 +3,22 @@ import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
 import del from '../assets/delete.png'
 
-const Items = ({text, id, isDone, remove, toggle}) => {
+const Items = ({text, desc, id, isDone, remove, toggle}) => {
   return (
+    <div className='bg-blue-100	p-3 m-3 rounded-lg'>
     <div className='flex items-center my-3 gap-2'>
         <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
-            <img className='w-7' src={isDone ? tick : not_tick}/>
-            <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${isDone ? "line-through" : ""}` }>{text}</p>
+            <img className='w-6' src={isDone ? tick : not_tick}/>
+            <p className={`ml-4 text-[17px] decoration-slate-500 ${isDone ? "line-through" : ""}` }>{text}</p>
         </div>
 
-        <img className='w-3.5 cursor-pointer' onClick={()=>{remove(id)}} src={del}/>
+        <img className='w-4 cursor-pointer' onClick={()=>{remove(id)}} src={del}/>
+    </div>
+    <div className='flex bg-gray-100 text-slate-700	min-h-10  items-center rounded-md w-full p-4'>
+          {desc && (
+          <div>{desc}</div>
+          )}
+    </div>
     </div>
   )
 }
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -5,22 +5,26 @@ const Todo = () => {
   const [todoList, seTodoList] = useState(localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []);
 
   const inputRef = useRef();
+  const decRef = useRef();
 
   const add = () => {
     const inputText = inputRef.current.value.trim();
+    const inputDesc = decRef.current.value.trim();
 
-    if (inputText === ""){
+    if (inputText === "" || inputDesc === ""){
       return null;
     }
 
     const newTodo = {
       id: Date.now(),
       text: inputText,
+      desc: inputDesc,
       isDone: false,
-    }
+    };
     seTodoList((prev)=>[...prev, newTodo]);
     inputRef.current.value = "";
-  }
+    decRef.current.value = "";
+  };
 
   const remove = (id) => {
     seTodoList((prvTodo)=>{
@@ -46,21 +50,29 @@ const Todo = () => {
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md
     flex flex-col p-7 min-h-[550px] rounded-xl'>
-
-    <div className='flex items-center mt mt-7 gap-2'>
+  
+    <div className='flex items-center mt mt-5 gap-2'>
       <img className='w-8' src={todo_icon}/>
       <h1 className='text-3xl font-semibold'>Task-Manager</h1>
     </div>
 
-    <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+    <div className='flex items-center mt-7 mb-3 bg-gray-200 rounded-full'>
       <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2
       placeholder:text-slate-600' type='text' placeholder='Add your task'/>
       <button onClick={add} className='border-none rounded-full bg-blue-500 text-white w-32 h-14 text-lg font-medium cursor-pointer'>Add +</button>
     </div>
+    <div className='bg-gray-200 rounded-lg'>
+      <input
+        ref={decRef}
+        className='bg-transparent border-0 outline-none w-full h-14 pl-6 pr-2 placeholder:text-slate-600 rounded-full'
+        type="text"
+        placeholder="Description of the task..."
+      />
+    </div>
 
     <div>
     {todoList.map((item, index)=>{
-      return <Items key={index} text={item.text} id={item.id} isDone={item.isDone} remove={remove} toggle={toggle}/>
+      return <Items key={index} text={item.text} desc={item.desc}id={item.id} isDone={item.isDone} remove={remove} toggle={toggle}/>
     })}
     </div>
     </div>
