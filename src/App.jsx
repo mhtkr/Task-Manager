39c95d71292ffc32c94a684005818f3a12c1e23c
M O H I T  K U M A R <mohitkr.9933@gmail.com>
2024-12-03 23:49:13 +0530

    Change app background color

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Todo from './Components/Todo'
 
 const App = () => {
   return (
-    <div className='bg-slate-900	grid py-4 min-h-screen'>
+    <div className='bg-gray-200 grid py-4 min-h-screen'>
       <Todo/>
     </div>
   )
